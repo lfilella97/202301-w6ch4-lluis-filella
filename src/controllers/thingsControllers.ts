018010c52d65d707b0dc891b6a0a502a60b80c9a
lfilella97 <lfilella97@gmail.com>
2023-02-16 23:01:02 +0100

    Create new debug on controllers

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -1,7 +1,8 @@
 import debug from "debug";
 import { type Request, type Response } from "express";
 import { things } from "../data/things.js";
-import { createDebug } from "../index.js";
+
+export const createDebug = debug("controller");
 
 export const getThings = (req: Request, res: Response) => {
   createDebug("Response things Send");
